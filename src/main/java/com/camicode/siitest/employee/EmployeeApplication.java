a5cb56566d6679832109463370e83c2a24cd096b
Camilo Martinez <camilomrtz1998@gmail.com>
2023-10-11 13:34:24 -0500

    add cors configuration

--- src/main/java/com/camicode/siitest/employee/EmployeeApplication.java
+++ src/main/java/com/camicode/siitest/employee/EmployeeApplication.java
@@ -2,6 +2,9 @@ package com.camicode.siitest.employee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EmployeeApplication {
@@ -9,5 +12,4 @@ public class EmployeeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeApplication.class, args);
 	}
-
 }
--- src/main/java/com/camicode/siitest/employee/controller/EmployeeController.java
+++ src/main/java/com/camicode/siitest/employee/controller/EmployeeController.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.camicode.siitest.employee.models.entity.Employee;
@@ -40,7 +42,7 @@ public class EmployeeController {
 		return emplService.findAll();
 	}
 	
-	@GetMapping("/employee/{id}")
+	@GetMapping("/employees/{id}")
 	public Optional<Employee> findOne(@PathVariable Integer id) {
 		Integer monthSalary;
 		Integer anualSalary;
@@ -56,6 +58,7 @@ public class EmployeeController {
 	}
 	
 	@PostMapping("/new-employee")
+	@ResponseStatus(HttpStatus.CREATED)
 	public ResponseEntity<Employee> newEmployee(@RequestBody Employee empl) {
 		emplService.save(empl);
 		getEmployeeAnualSalary(empl.id);
